0f78304f8a85d7c9e7d87f6fe8be53b95e33cd6a
DYBEDY <rd.iosdeveloper@gmail.com>
2021-12-24 23:24:26 +0300

    Initial Commit

---
+++ HomeWork2.2/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  HomeWork2.2
+//
+//  Created by Roman on 10.12.2021.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    @IBOutlet var resultOfSettingsView: UIView!
+    
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redColorTextField: UITextField!
+    @IBOutlet var greenColorTextField: UITextField!
+    @IBOutlet var blueColorTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        resultOfSettingsView.layer.cornerRadius = 30
+        
+        maxValuesOfSliders()
+        
+        
+        redSlider.value = 255
+        greenSlider.value = 19
+        blueSlider.value = 10
+        
+        redColorTextField.delegate = self
+        greenColorTextField.delegate = self
+        blueColorTextField.delegate = self
+        
+        maxTintColor()
+        minTintColor()
+        
+        setColor()
+        setValue(for: redValueLabel, greenValueLabel, blueValueLabel)
+    }
+    
+    
+    @IBAction func doneButtonPressed() {
+        dismiss(animated: true)
+    }
+    
+    @IBAction func sliderAction(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider: setValue(for: redValueLabel)
+        case greenSlider: setValue(for: greenValueLabel)
+        default: setValue(for: blueValueLabel)
+        }
+       
+        }
+    
+    //MARK: - Private methods
+    
+    private func setColor() {
+        resultOfSettingsView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value / 255),
+            green: CGFloat(greenSlider.value / 255),
+            blue: CGFloat(blueSlider.value / 255), alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case redValueLabel:
+                label.text = string(from: redSlider )
+                redColorTextField.text = label.text
+            case greenValueLabel:
+                label.text = string(from: greenSlider)
+                greenColorTextField.text = label.text
+            default:
+                label.text = string(from: blueSlider)
+                blueColorTextField.text = label.text
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.0f", slider.value )
+    }
+    
+    private func maxValuesOfSliders() {
+        redSlider.maximumValue = 255
+        greenSlider.maximumValue = 255
+        blueSlider.maximumValue = 255
+    }
+    
+    private func maxTintColor() {
+        redSlider.maximumTrackTintColor = .white
+        greenSlider.maximumTrackTintColor = .white
+        blueSlider.maximumTrackTintColor = .white
+    }
+    
+    private func minTintColor() {
+        redSlider.minimumTrackTintColor = . red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+}
+
+// MARK: - Keyboard methods
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let newValue = textField.text else { return }
+        guard let newNUmber = Float(newValue) else { return }
+        
+        if textField == redColorTextField {
+            redSlider.value = newNUmber
+            redValueLabel.text = newValue
+            
+        } else if textField == greenColorTextField {
+            greenSlider.value = newNUmber
+            greenValueLabel.text = newValue
+        } else {
+            blueSlider.value = newNUmber
+            blueValueLabel.text = newValue
+        }
+        setColor()
+    }
+}
+
+            
+    
+            
---
+++ HomeWork2.2/ViewControllers/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  HomeWork2.2
+//
+//  Created by Roman on 24.12.2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+
+  
+}
